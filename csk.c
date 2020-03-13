e618033604322c9f18ca2cb17c3eba22ced19afe
Cedric MARCHAND <cedric.marchand@univ-ubs.fr>
2020-03-13 10:51:35 +0100

    update 64CCSK, 256CCSK, add 64APSK_CCSK256

--- csk.c
+++ csk.c
@@ -149,9 +149,9 @@ void PNGenerator( csk_t *csk)
             for(i=0; i<csk->PNsize; i++)
             {
                 csk->PN[i] =PN1024[i];
-                printf(" %d ",csk->PN[i]);
+                //printf(" %d ",csk->PN[i]);
             }
-            getchar();
+            //getchar();
         }
 
 
@@ -618,11 +618,12 @@ void ModelChannel_AWGN_64_CSK(csk_t *csk,code_t *code, decoder_t *decoder, int *
     };
 
 
+
     ////compute normalization factor so that average power of one point of constellation is equal to one
     float norm_factor=0.0;
     for(i=0; i < code->GF ; i++)
     {
-        // norm_factor = table_64QAM[i][0]*table_64QAM[i][0] + table_64QAM[i][1]*table_64QAM[i][1]+norm_factor;//compute sum
+        //norm_factor = table_64QAM[i][0]*table_64QAM[i][0] + table_64QAM[i][1]*table_64QAM[i][1]+norm_factor;//compute sum
         norm_factor = table_64APSK[i][0]*table_64APSK[i][0] + table_64APSK[i][1]*table_64APSK[i][1]+norm_factor;//compute sum
     }
     norm_factor = sqrt( code->GF / norm_factor);
@@ -663,21 +664,45 @@ void ModelChannel_AWGN_64_CSK(csk_t *csk,code_t *code, decoder_t *decoder, int *
             TMP[k] =0;
         }
 
-        for (g=0; g<csk->PNsize; g++)
+
+        if (n<N) // per symbol puncturing
         {
+
             for(k=0; k<code->GF; k++)
             {
-                som=0;
-
-                for (q=0; q<6; q++)
+                for (g=0; g<csk->PNsize; g++)
                 {
-                    som = som + BinGF_64[k][q]*pow(2,q);
+
+                    som=0;
+
+                    for (q=0; q<6; q++)
+                    {
+                        som = som + BinGF_64[k][q]*pow(2,q);
+                    }
+                    TMP[k] = TMP[k]+SQR(NoisyBin[g][0]-modulation[CCSK64[som+g]][0])/(2.0*SQR(sigma))+SQR(NoisyBin[g][1]-modulation[CCSK64[som+g]][1])/(2.0*SQR(sigma));
+                    //printf("%d %f \n",k, TMP[k]);
                 }
-                TMP[k] = TMP[k]+SQR(NoisyBin[g][0]-modulation[CCSK64[som+g]][0])/(2.0*SQR(sigma))+SQR(NoisyBin[g][1]-modulation[CCSK64[som+g]][1])/(2.0*SQR(sigma));
-                //printf("%d %f \n",k, TMP[k]);
             }
         }
+        else
+        {
 
+            for(k=0; k<code->GF; k++)
+            {
+                for (g=0; g<csk->PNsize-1; g++)
+                {
+
+                    som=0;
+
+                    for (q=0; q<6; q++)
+                    {
+                        som = som + BinGF_64[k][q]*pow(2,q);
+                    }
+                    TMP[k] = TMP[k]+SQR(NoisyBin[g][0]-modulation[CCSK64[som+g]][0])/(2.0*SQR(sigma))+SQR(NoisyBin[g][1]-modulation[CCSK64[som+g]][1])/(2.0*SQR(sigma));
+                    //printf("%d %f \n",k, TMP[k]);
+                }
+            }
+        }
 
         //getchar();
         for(k=0; k<code->GF; k++)
@@ -813,6 +838,30 @@ void ModelChannel_AWGN_256_CSK(csk_t *csk,code_t *code, decoder_t *decoder, int 
 //
 //    };
 
+//    const int CCSK256[512]={56, 107, 10, 45, 108, 177, 227, 194, 22, 98, 236, 36, 66, 99, 234, 82, 62, 237,
+//     161, 198, 83, 252, 119, 239, 104, 151, 6, 202, 64, 157, 144, 78, 43, 253, 152, 84, 243, 168, 229, 129, 77, 224, 213, 187, 254, 29,
+//     175, 75, 154, 28, 170, 135, 199, 55, 225, 127, 32, 128, 5, 102, 65, 126, 114, 74, 35, 54, 130, 200, 46, 146, 211, 49, 110, 172, 94,
+//     191, 121, 72, 141, 38, 14, 118, 91, 87, 183, 52, 134, 116, 180, 142, 18, 31, 188, 81, 51, 122, 53, 186, 164, 23, 255, 185, 206, 79, 217,
+//      181, 133, 25, 250, 16, 233, 136, 228, 249, 21, 92, 232, 12, 189, 167, 89, 50, 100, 140, 238, 148, 70, 245, 37, 223, 8, 179, 85, 44, 219,
+//       113, 220, 201, 58, 30, 93, 176, 57, 158, 155, 88, 17, 203, 212, 208, 147, 24, 150, 131, 138, 90, 159, 197, 0, 153, 247, 244, 207, 156,
+//       69, 231, 178, 117, 105, 80, 41, 34, 137, 195, 26, 242, 86, 60, 184, 222, 173, 95, 165, 143, 3, 7, 125, 210, 19, 209, 221, 193, 160, 2,
+//       48, 47, 139, 241, 218, 248, 163, 226, 76, 115, 27, 20, 4, 251, 42, 111, 182, 192, 101, 162, 103, 39, 132, 61, 169, 205, 73, 97, 196, 190,
+//       71, 171, 230, 214, 68, 13, 112, 145, 124, 1, 174, 215, 123, 67, 106, 96, 246, 240, 40, 33, 109, 15, 149, 235, 59, 63, 216, 11, 204, 9,
+//       120, 166,
+//       130, 74, 86, 16, 234, 15, 87, 80, 118, 223, 97, 27, 198, 9, 153, 103, 62, 60, 2, 210, 196, 197, 184, 66, 139, 26, 53, 145, 151, 208, 119, 96,
+//75, 220, 163, 147, 10, 91, 61, 207, 149, 48, 195, 152, 22, 177, 105, 238, 182, 143, 20, 95, 63, 248, 225, 18, 132, 52, 222, 90, 172, 133, 70,
+//69, 237, 137, 40, 157, 47, 64, 44, 111, 174, 201, 39, 49, 219, 169, 235, 55, 199, 121, 251, 71, 215, 156, 13, 54, 144, 154, 200, 230, 94, 247,
+//1, 242, 30, 244, 32, 56, 236, 193, 167, 240, 214, 46, 155, 136, 146, 178, 6, 253, 212, 218, 99, 173, 25, 164, 43, 129, 188, 7, 59, 190, 24, 38,
+//11, 114, 203, 14, 221, 35, 171, 131, 122, 73, 148, 37, 36, 192, 51, 93, 45, 128, 42, 216, 209, 176, 175, 34, 79, 88, 135, 78, 202, 12, 227, 141,
+// 231, 134, 106, 217, 0, 58, 232, 82, 72, 161, 92, 252, 102, 109, 98, 150, 185, 81, 23, 31, 160, 110, 124, 3, 65, 83, 213, 50, 246, 243, 186, 159,
+//  100, 179, 205, 194, 140, 123, 255, 249, 245, 68, 187, 206, 162, 107, 181, 76, 17, 165, 104, 112, 166, 33, 254, 57, 241, 8, 120, 158, 170, 168,
+//   115, 126, 183, 101, 127, 85, 28, 125, 117, 224, 41, 204, 67, 189, 138, 116, 226, 5, 250, 142, 233, 191, 108, 84, 180, 77, 113, 21, 4, 239, 89,
+//    228, 229, 211, 29, 19
+//
+//       };
+
+
+
 
 
 float table_GF[code->GF][2];
@@ -869,37 +918,71 @@ float table_GF[code->GF][2];
         }
         //printf("\nGF: %d \n",som );
 
-        for (q=0; q<csk->PNsize; q++)
+                     for(k=0; k<code->GF; k++)
+                    {
+                        TMP[k] =0;
+                    }
+
+        if (n<N/2)
         {
-            for (i=0; i<2; i++)
-            {
-                u=My_drand48(init_rand);
-                v=My_drand48(init_rand);
-                /* BPSK modulation + AWGN noise (Box Muller method for Gaussian sampling) */
-                NoisyBin[q][i] = modulation[CCSK256[(som+q)%256]][i]+ sigma*sqrt(-2.0*log(u))*cos(2.0*PI*v)  ;
-            }
+
+
+                    for (q=0; q<csk->PNsize; q++)
+                    {
+                        for (i=0; i<2; i++)
+                        {
+                            u=My_drand48(init_rand);
+                            v=My_drand48(init_rand);
+                            /* BPSK modulation + AWGN noise (Box Muller method for Gaussian sampling) */
+                            NoisyBin[q][i] = modulation[CCSK256[(som+q)%256]][i]+ sigma*sqrt(-2.0*log(u))*cos(2.0*PI*v)  ;
+                        }
+                    }
+
+                    for (g=0; g<csk->PNsize; g++)
+                    {
+                        for(k=0; k<code->GF; k++)
+                        {
+                            som=0;
+
+                            for (q=0; q<8; q++)
+                            {
+                                som = som + BinGF_256[k][q]*pow(2,q);
+                            }
+                            TMP[k] = TMP[k]+SQR(NoisyBin[g][0]-modulation[CCSK256[(som+g)%256]][0])/(2.0*SQR(sigma))+SQR(NoisyBin[g][1]-modulation[CCSK256[(som+g)%256]][1])/(2.0*SQR(sigma));
+                            //printf("%d %f \n",k, TMP[k]);
+                        }
+                    }
+        }
+        else
+        {
+
+                    for (q=0; q<csk->PNsize; q++)
+                    {
+                        for (i=0; i<2; i++)
+                        {
+                            u=My_drand48(init_rand);
+                            v=My_drand48(init_rand);
+                            /* BPSK modulation + AWGN noise (Box Muller method for Gaussian sampling) */
+                            NoisyBin[q][i] = modulation[CCSK256[(som+q)%256]][i]+ sigma*sqrt(-2.0*log(u))*cos(2.0*PI*v)  ;
+                        }
+                    }
+
+                    for (g=0; g<csk->PNsize-1; g++)
+                    {
+                        for(k=0; k<code->GF; k++)
+                        {
+                            som=0;
+
+                            for (q=0; q<8; q++)
+                            {
+                                som = som + BinGF_256[k][q]*pow(2,q);
+                            }
+                            TMP[k] = TMP[k]+SQR(NoisyBin[g][0]-modulation[CCSK256[(som+g)%256]][0])/(2.0*SQR(sigma))+SQR(NoisyBin[g][1]-modulation[CCSK256[(som+g)%256]][1])/(2.0*SQR(sigma));
+                            //printf("%d %f \n",k, TMP[k]);
+                        }
+                    }
         }
 
-
-         for(k=0; k<code->GF; k++)
-        {
-            TMP[k] =0;
-        }
-
-        for (g=0; g<csk->PNsize; g++)
-        {
-            for(k=0; k<code->GF; k++)
-            {
-                som=0;
-
-                for (q=0; q<8; q++)
-                {
-                    som = som + BinGF_256[k][q]*pow(2,q);
-                }
-                TMP[k] = TMP[k]+SQR(NoisyBin[g][0]-modulation[CCSK256[(som+g)%256]][0])/(2.0*SQR(sigma))+SQR(NoisyBin[g][1]-modulation[CCSK256[(som+g)%256]][1])/(2.0*SQR(sigma));
-                //printf("%d %f \n",k, TMP[k]);
-            }
-        }
 
 
         //getchar();
@@ -913,6 +996,137 @@ float table_GF[code->GF][2];
     free(NoisyBin);
 }
 
+// NG(256) is mapped on 64NB-CCSK
+void ModelChannel_AWGN_64APSK_CSK256(csk_t *csk,code_t *code, decoder_t *decoder, int **NBIN, float EbN, int *init_rand)
+{
+    const int N = code->N;
+    int n,k,g,q;
+    float u,v,sigma;
+    float TMP[code->GF];
+    int som;
+
+
+    float **NoisyBin = calloc(csk->PNsize,sizeof(float *));
+    for (q=0; q<csk->PNsize; q++) NoisyBin[q] = calloc(2,sizeof(float));
+    /* Binary-input AWGN channel : */
+
+    int i;
+
+    float modulation[64][2];
+
+    const int CCSK64[256]={
+
+            2,57,33,29,26,4,58,10,27,43,37,7,19,20,0,63,6,51,8,36,9,62,53,15,18,23,5,34,13,55,45,16,46,35,48,21,61,22,40,12,56,47,24,1,39,
+    41,42,44,38,59,32,49,30,17,50,52,11,54,14,3,60,31,25,28,
+
+    0,21,61,48,2,8,57,49,19,14,62,53,16,6,20,45,22,24,26,54,28,30,1,5,59,10,60,43,55,32,11,46,18,34,52,36,9,38,12,58,40,
+    23,25,50,27,42,13,51,29,31,56,4,33,17,44,3,63,35,37,39,41,15,47,7,
+
+32, 40, 22, 34, 35, 6, 55, 3, 16, 11, 54, 30, 45, 60, 62, 51, 33, 7, 38, 58, 42, 28, 17, 41, 47, 14, 46, 56, 63, 8, 59, 5,
+ 48, 53, 29, 21, 25, 52, 37, 64, 31, 49, 27, 61, 50, 26, 43, 19, 44, 15, 1, 36, 23, 2, 4, 18, 24, 39, 13, 9, 20, 57, 10, 12,
+
+59, 35, 28, 27, 55, 48, 63, 57, 32, 4, 5, 51, 37, 41, 13, 49, 10, 14, 8, 6, 43, 22, 39, 36, 12, 17, 25, 64, 56, 47, 34, 16,
+40, 29, 53, 3, 20, 26, 33, 19, 42, 15, 44, 45, 46, 24, 23, 60, 30, 38, 9, 61, 52, 18, 7, 62, 1, 50, 21, 11, 31, 2, 58, 54
+
+    };
+
+
+
+    ////compute normalization factor so that average power of one point of constellation is equal to one
+    float norm_factor=0.0;
+    for(i=0; i < 64 ; i++)
+    {
+        //norm_factor = table_64QAM[i][0]*table_64QAM[i][0] + table_64QAM[i][1]*table_64QAM[i][1]+norm_factor;//compute sum
+        norm_factor = table_64APSK[i][0]*table_64APSK[i][0] + table_64APSK[i][1]*table_64APSK[i][1]+norm_factor;//compute sum
+    }
+    norm_factor = sqrt( 64 / norm_factor);
+// printf(" norm_factor = %f ", norm_factor); getchar();
+
+    for(i=0; i< 64 ; i++)
+    {
+        modulation[i][0]=norm_factor*table_64APSK[i][0];
+        modulation[i][1]=norm_factor*table_64APSK[i][1];
+    }
+
+
+    sigma = sqrt(1.0/(2.0*pow(10,EbN/10.0)));
+
+    for (n=0; n<N; n++)
+    {
+        som=0;
+
+        for (q=0; q<8; q++)
+        {
+            som = som + NBIN[n][q]*pow(2,q);
+        }
+        //printf("\n %d \n",som );
+
+        for (q=0; q<csk->PNsize; q++)
+        {
+            for (i=0; i<2; i++)
+            {
+                u=My_drand48(init_rand);
+                v=My_drand48(init_rand);
+                /* BPSK modulation + AWGN noise (Box Muller method for Gaussian sampling) */
+                NoisyBin[q][i] = modulation[CCSK64[(som+q)%256]][i]+ sigma*sqrt(-2.0*log(u))*cos(2.0*PI*v)  ;
+            }
+        }
+
+         for(k=0; k<code->GF; k++)
+        {
+            TMP[k] =0;
+        }
+
+
+        if (n<N) // per symbol puncturing
+        {
+
+            for(k=0; k<code->GF; k++)
+            {
+                for (g=0; g<csk->PNsize; g++)
+                {
+
+                    som=0;
+
+                    for (q=0; q<8; q++)
+                    {
+                        som = som + BinGF_256[k][q]*pow(2,q);
+                    }
+                    TMP[k] = TMP[k]+SQR(NoisyBin[g][0]-modulation[CCSK64[(som+g)%256]][0])/(2.0*SQR(sigma))+SQR(NoisyBin[g][1]-modulation[CCSK64[(som+g)%256]][1])/(2.0*SQR(sigma));
+                    //printf("%d %f \n",k, TMP[k]);
+                }
+            }
+        }
+//        else
+//        {
+//
+//            for(k=0; k<code->GF; k++)
+//            {
+//                for (g=0; g<csk->PNsize-1; g++)
+//                {
+//
+//                    som=0;
+//
+//                    for (q=0; q<6; q++)
+//                    {
+//                        som = som + BinGF_64[k][q]*pow(2,q);
+//                    }
+//                    TMP[k] = TMP[k]+SQR(NoisyBin[g][0]-modulation[CCSK64[som+g]][0])/(2.0*SQR(sigma))+SQR(NoisyBin[g][1]-modulation[CCSK64[som+g]][1])/(2.0*SQR(sigma));
+//                    //printf("%d %f \n",k, TMP[k]);
+//                }
+//            }
+//        }
+
+        //getchar();
+        for(k=0; k<code->GF; k++)
+        {
+            decoder->intrinsic_LLR[n][k] = TMP[k];
+        }
+    }
+
+    for (q=0; q<csk->PNsize; q++) free(NoisyBin[q]);
+    free(NoisyBin);
+}
 
 
 
